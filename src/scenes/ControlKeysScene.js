a4aa9233ebc52147842ad3548e34226c6bf0c530
miniponti <61016923+miniponti@users.noreply.github.com>
2020-12-04 18:37:08 +0100

    pequeña mejora a la escena de controles
    se me habia olvidado poner el fondo del titulo de manera que parezca que está superpuesta a la escena del titulo

--- src/scenes/ControlKeysScene.js
+++ src/scenes/ControlKeysScene.js
@@ -7,11 +7,13 @@ class ControlKeysScene extends Phaser.Scene{
         console.log("CONTROL_KEYS_SCENE PRELOAD");
 
         this.load.image('CONTROLS_MENU','assets/interface/CONTROLS_MENU.png');
+        this.load.image('CONTROLS_BG','assets/sprites/TITLE_BACKGROUND.png');
         this.load.image('EXIT_BUTTON','assets/sprites/buttons/EXIT_BUTTON.png');
     }
 
     create(){
         console.log("CONTROL_KEYS_SCENE CREATE");
+        this.add.sprite(0,0,'CONTROLS_BG').setOrigin(0,0);
         this.add.sprite(config.width/2, config.height/2, 'CONTROLS_MENU');
 
         //BOTON SALIDA
