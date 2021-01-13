7174fd70a8fcf6c80eca1eeeab5f21a9bbb2fe7a
dakeXd <47038301+dakeXd@users.noreply.github.com>
2021-01-13 21:44:11 +0100

    ARchivos del sevidor

---
+++ Mensajes/java/com/example/demo/Mensajes2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mensajes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mensajes2Application.class, args);
+		
+		
+	}
+
+}
